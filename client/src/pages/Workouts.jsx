d7eb90b5428d4ecc3893cb03366d118e432ce47c
rizi2327 <mrmrkhan10000@gmail.com>
2025-12-07 16:19:03 +0500

    feat: implement workout fetching and loading state in Workouts component

--- client/src/pages/Workouts.jsx
+++ client/src/pages/Workouts.jsx
@@ -1,122 +1,129 @@
-import React from 'react'
-import styled from 'styled-components'
-import WorkoutCard from '../components/Cards/WorkoutCard';
-import {LocalizationProvider} from "@mui/x-date-pickers/LocalizationProvider";
-import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
-import {DateCalendar} from "@mui/x-date-pickers";
-const Container=styled.div`
-flex:1;
-height:100%;
-display:flex;
-justify-content:center;
-padding:22px 0px;
-overflow-y:scroll;
-`;
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import WorkoutCard from "../components/Cards/WorkoutCard";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers";
+import { getWorkouts } from "../api";
+import { CircularProgress } from "@mui/material";
+import { useDispatch } from "react-redux";
 
-const Wrapper=styled.div`
-flex:1;
-max-width:1600px;
-padding:0px 16px;
-display:flex;
-gap:16px;
-@media (max-width:600px)
-{
-gap:12px;
-flex-direction:column;
-}
+const Container = styled.div`
+  flex: 1;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 22px 0px;
+  overflow-y: scroll;
 `;
-
-const Left=styled.div`
-flex:0.3;
-height:fit-content;
-padding:18px;
-border:1px solid ${({theme})=>theme.text_primary};
-border-radius:14px;
-margin-bottom:20px;
-box-shadow:1px 4px 8px 0px ${({theme})=>theme.text_secondary};
-position:sticky;
-top:20px;
-@media(max-width:600px)
-{
-position:static;
-flex:1;
-}
+const Wrapper = styled.div`
+  flex: 1;
+  max-width: 1600px;
+  display: flex;
+  gap: 22px;
+  padding: 0px 16px;
+  @media (max-width: 600px) {
+    gap: 12px;
+    flex-direction: column;
+  }
 `;
-const Title=styled.div`
-font-weight:600;
-font-size:16px;
-color: ${({theme})=>theme.primary};
-margin-bottom:12px;
-@media(max-width:600px)
-{
-font-size:14px;
-}`;
-
-const Right=styled.div`
-flex:0.7;
-display:flex;
-flex-direction:column
-@media(max-width:600px)
-{
-flex:1;
-}
+const Left = styled.div`
+  flex: 0.2;
+  height: fit-content;
+  padding: 18px;
+  border: 1px solid ${({ theme }) => theme.text_primary + 20};
+  border-radius: 14px;
+  box-shadow: 1px 6px 20px 0px ${({ theme }) => theme.primary + 15};
 `;
-
-const Section=styled.div`
-display:flex;
-flex-direction:column;
-padding:0px 16px;
-gap:22px;
-@media(max-width:600px)
-{
-gap:12px;
-}
+const Title = styled.div`
+  font-weight: 600;
+  font-size: 16px;
+  color: ${({ theme }) => theme.primary};
+  @media (max-width: 600px) {
+    font-size: 14px;
+  }
 `;
-
-const SectionTitle=styled.div`
-font-weight:600;
-font-size:22px;
-color: ${({theme})=>theme.text_primary};
-margin-bottom:10px;
+const Right = styled.div`
+  flex: 1;
 `;
-
-const CardWrapper=styled.div`
-display:flex;
-flex-wrap:wrap;
-justify-content:flex-start;
-gap:20px;
-margin-bottom:100px;
-@media(max-width:600px)
-{
-gap:12px;
-justify-content:center;
-}
+const CardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+  margin-bottom: 100px;
+  @media (max-width: 600px) {
+    gap: 12px;
+  }
+`;
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0px 16px;
+  gap: 22px;
+  padding: 0px 16px;
+  @media (max-width: 600px) {
+    gap: 12px;
+  }
+`;
+const SecTitle = styled.div`
+  font-size: 22px;
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
 `;
 
 const Workouts = () => {
+  const dispatch = useDispatch();
+  const [todaysWorkouts, setTodaysWorkouts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [date, setDate] = useState("");
+
+  const getTodaysWorkout = async () => {
+    setLoading(true);
+    const token = localStorage.getItem("fittrack-app-token");
+    try {
+      const res = await getWorkouts(token, date ? `?date=${date}` : "");
+      setTodaysWorkouts(res?.data?.todayWorkouts ?? []);
+      console.log(res.data);
+    } catch (err) {
+      console.error('getTodaysWorkout error', err);
+      setTodaysWorkouts([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getTodaysWorkout();
+  }, [date]);
   return (
     <Container>
       <Wrapper>
         <Left>
           <Title>Select Date</Title>
-          <LocalizationProvider dateAdapter={AdapterDayjs} >
-            <DateCalendar/>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateCalendar
+              onChange={(e) => setDate(`${e.$M + 1}/${e.$D}/${e.$y}`)}
+            />
           </LocalizationProvider>
         </Left>
         <Right>
           <Section>
-            <SectionTitle>Today's Workout</SectionTitle>
-            <CardWrapper>
-              <WorkoutCard/>
-              <WorkoutCard/>
-              <WorkoutCard/>
-              <WorkoutCard/>
-            </CardWrapper>
+            <SecTitle>Todays Workout</SecTitle>
+            {loading ? (
+              <CircularProgress />
+            ) : (
+              <CardWrapper>
+                {todaysWorkouts.map((workout) => (
+                  <WorkoutCard workout={workout} />
+                ))}
+              </CardWrapper>
+            )}
           </Section>
         </Right>
       </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Workouts
+export default Workouts;
